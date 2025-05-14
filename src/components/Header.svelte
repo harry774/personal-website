58f4c8444ccd320ccee0fef7a5f57de79b1b47e6
harry774 <xylo.hardik@gmail.com>
2025-05-13 22:17:24 -0400

    Added components, layout page, fonts, and particle json

---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let y;
+
+    let tabs = [
+        { name: "Projects", link: "#projects" },
+        { name: "About me", link: "#about" },
+        //{ name: "Projects", link: "/projects" },
+    ];
+</script>
+
+<header>
+    <h1>
+        <b>Hardik</b> Patel
+    </h1>
+    <div class="">
+        {#each tabs as tab, index}
+            <a>
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+    </div>
+</header>
